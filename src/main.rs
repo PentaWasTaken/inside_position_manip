90379630d5393588fb5ec6bbd385d8135c751113
Piet Helzel <piet.helzel@outlook.com>
2021-05-14 02:22:26 +0200

    Basic interface to the overlay

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod api;
+mod overlay;
+
+use api::APIHandle;
+use overlay::Overlay;
+
 fn main() {
-    println!("Hello, world!");
+    //Create a handle to the process
+    let process_handle = APIHandle::new().unwrap();
+
+    let overlay = Overlay::new();
+
+    overlay.run(process_handle);
 }
