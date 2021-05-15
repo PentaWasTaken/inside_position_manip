84f5ba8f27b5414dcfdeb49cac8b62a232b8f111
Piet Helzel <piet.helzel@outlook.com>
2021-05-15 22:13:43 +0200

    Nothing changed really :(

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 mod api;
 mod overlay;
 
+use std::borrow::Borrow;
+
 use api::APIHandle;
 use overlay::Overlay;
 
+use std::convert::TryInto;
+
 fn main() {
     //Create a handle to the process
-    let process_handle = APIHandle::new().expect("Could not create handle to target process.");
+    let api_handle = APIHandle::new().expect("Could not create handle to target process.");
 
-    let overlay = Overlay::new((500, 500));
+    let overlay = Overlay::new((500, 300));
 
-    overlay.run(process_handle);
+    overlay.run(api_handle);
 }
