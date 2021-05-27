10e9d0c2a56225060b589c1e7d657f9632ff5d6e
Piet Helzel <piet.helzel@outlook.com>
2021-05-27 23:55:29 +0200

    Changed overlay size to fit better

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,15 @@
 mod api;
+mod input_handler;
 mod overlay;
-
-use std::borrow::Borrow;
 
 use api::APIHandle;
 use overlay::Overlay;
-
-use std::convert::TryInto;
 
 fn main() {
     //Create a handle to the process
     let api_handle = APIHandle::new().expect("Could not create handle to target process.");
 
-    let overlay = Overlay::new((500, 300));
+    let overlay = Overlay::new((500, 200));
 
     overlay.run(api_handle);
 }
