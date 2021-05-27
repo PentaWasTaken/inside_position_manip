2e5a8f9640a83938f0e614bb4aa323afbec6ddbb
Piet Helzel <piet.helzel@outlook.com>
2021-05-27 23:55:07 +0200

    Added functionality to write memory

--- src/api.rs
+++ src/api.rs
@@ -1,13 +1,15 @@
 use std::ffi::{c_void, CString};
 use std::mem::{size_of, size_of_val};
+use std::ptr;
 
 use winapi::shared::windef::{HWND, LPRECT, RECT};
 use winapi::um::dwmapi::{DwmGetWindowAttribute, DWMWA_EXTENDED_FRAME_BOUNDS};
+use winapi::um::errhandlingapi::GetLastError;
 use winapi::um::memoryapi::{ReadProcessMemory, WriteProcessMemory};
 use winapi::um::processthreadsapi::OpenProcess;
 use winapi::um::psapi::EnumProcessModules;
-use winapi::um::winnt::{HANDLE, PROCESS_QUERY_INFORMATION, PROCESS_VM_READ};
-use winapi::um::winuser::{FindWindowA, GetWindowThreadProcessId};
+use winapi::um::winnt::{HANDLE, PROCESS_QUERY_INFORMATION, PROCESS_VM_READ, PROCESS_VM_WRITE};
+use winapi::um::winuser::{FindWindowA, GetForegroundWindow, GetWindowThreadProcessId};
 
 pub struct APIHandle {
     window_handle: HWND,
@@ -15,27 +17,32 @@ pub struct APIHandle {
     pub base_address: usize,
 }
 
+#[allow(temporary_cstring_as_ptr)]
 impl APIHandle {
     pub fn new() -> Result<Self, ()> {
         unsafe {
             let window_handle = FindWindowA(
-                std::ptr::null(),
+                ptr::null(),
                 CString::new("INSIDE").unwrap().as_ptr() as *const i8,
             );
 
             let pid = &mut 0u32;
             GetWindowThreadProcessId(window_handle, pid as *mut u32);
 
-            let handle = OpenProcess(PROCESS_VM_READ | PROCESS_QUERY_INFORMATION, 0, *pid);
+            let handle = OpenProcess(PROCESS_VM_READ | PROCESS_QUERY_INFORMATION | PROCESS_VM_WRITE, 0, *pid);
 
             if handle.is_null() {
                 return Err(());
             }
 
-            let lpcbNeeded = &mut 0u32;
-            let mut module_buffer = vec![std::ptr::null_mut(); 1024];
+            let mut module_buffer = vec![ptr::null_mut(); 1024];
 
-            EnumProcessModules(handle, module_buffer.as_mut_ptr(), size_of_val(&module_buffer) as u32, lpcbNeeded as *mut _);
+            EnumProcessModules(
+                handle,
+                module_buffer.as_mut_ptr(),
+                size_of_val(&module_buffer) as u32,
+                ptr::null_mut(),
+            );
 
             Ok(APIHandle {
                 window_handle,
@@ -69,6 +76,17 @@ impl APIHandle {
         f32::from_le_bytes(bytes)
     }
 
+    pub fn write_memory_f32(&self, offsets: &[usize], val: f32) {
+        let address = self.get_final_address(offsets);
+        let buffer = val.to_le_bytes();
+
+        println!("{:x}", address);
+
+        unsafe {
+            WriteProcessMemory(self.handle, address as *mut _, buffer.as_ptr() as *const _, buffer.len(), ptr::null_mut());
+        }
+    }
+
     pub fn get_final_address(&self, offsets: &[usize]) -> usize {
         let mut base = self.base_address;
         for offset in offsets.iter().take(offsets.len() - 1) {
@@ -82,7 +100,13 @@ impl APIHandle {
     pub fn read_bytes<const N: usize>(&self, address: usize, bytes_to_read: usize) -> [u8; N] {
         let mut buffer = [0u8; N];
         unsafe {
-            ReadProcessMemory(self.handle, address as *const c_void, buffer.as_mut_ptr() as *mut _, bytes_to_read, std::ptr::null_mut());
+            ReadProcessMemory(
+                self.handle,
+                address as *const c_void,
+                buffer.as_mut_ptr() as *mut _,
+                bytes_to_read,
+                ptr::null_mut(),
+            );
         }
         buffer
     }
