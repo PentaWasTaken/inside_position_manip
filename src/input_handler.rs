1ad179e751c2dead6d6b424f62e141b5c9277554
Piet Helzel <piet.helzel@outlook.com>
2021-06-06 00:07:52 +0200

    Added x and y velocity

--- src/input_handler.rs
+++ src/input_handler.rs
@@ -9,6 +9,10 @@ const YPOS_OFFSETS: &[usize] = &[0xF92610, 0x4C0, 0x10, 0x98, 0x670, 0x0, 0x58, 
 
 const ZPOS_OFFSETS: &[usize] = &[0x1001FA0, 0x260, 0x2E8, 0x318, 0x10, 0x28, 0x40, 0x18];
 
+const XVEL_OFFSETS: &[usize] = &[0xF92610, 0x4C0, 0x10, 0x98, 0x670, 0x0, 0x58, 0x70, 0x1C];
+
+const YVEL_OFFSETS: &[usize] = &[0xF92610, 0x4C0, 0x10, 0x98, 0x670, 0x0, 0x58, 0x70, 0x20];
+
 const KEYS: &[Keycode] = &[
     Keycode::X,
     Keycode::Y,
@@ -41,6 +45,7 @@ const KEYS: &[Keycode] = &[
     Keycode::N,
     Keycode::Backspace,
     Keycode::C,
+    Keycode::P,
 ];
 
 enum PositionKey {
@@ -53,11 +58,17 @@ enum NumMode {
     Set,
 }
 
+enum Parameter {
+    Position,
+    Velocity,
+}
+
 pub struct InputHandler {
     device_state: DeviceState,
     prev_keys: Vec<Keycode>,
     position_key: PositionKey,
     num_mode: NumMode,
+    parameter: Parameter,
     input_text: String,
 }
 
@@ -68,22 +79,24 @@ impl InputHandler {
             prev_keys: Vec::new(),
             position_key: PositionKey::X,
             num_mode: NumMode::Set,
+            parameter: Parameter::Position,
             input_text: String::new(),
         }
     }
 
     pub fn get_next_key(&mut self) -> Option<Keycode> {
         let keys = self.device_state.get_keys();
-        let relevant_keys: Vec<Keycode> = keys
-            .into_iter()
-            .filter(|key| KEYS.contains(key))
+        let relevant_keys: Vec<Keycode> =
+            keys.into_iter().filter(|key| KEYS.contains(key)).collect();
+
+        let filtered_keys: Vec<&Keycode> = relevant_keys
+            .iter()
+            .filter(|&key| !self.prev_keys.contains(key))
             .collect();
 
-        let filtered_keys: Vec<&Keycode> = relevant_keys.iter().filter(|&key| !self.prev_keys.contains(key)).collect();
-
-        self.prev_keys = relevant_keys.iter().map(|key| key.clone()).collect();
-        if filtered_keys.len() > 0 {
-            Some(filtered_keys[0].clone().clone())
+        self.prev_keys = relevant_keys.to_vec();
+        if !filtered_keys.is_empty() {
+            Some(filtered_keys[0].clone())
         } else {
             None
         }
@@ -101,16 +114,28 @@ impl InputHandler {
             NumMode::Set => "SET",
         };
 
+        let parameter = match self.parameter {
+            Parameter::Position => "POS",
+            Parameter::Velocity => "VEL",
+        };
+
         let x_pos = api_handle.read_memory_f32(XPOS_OFFSETS);
         let y_pos = api_handle.read_memory_f32(YPOS_OFFSETS);
         let z_pos = api_handle.read_memory_f32(ZPOS_OFFSETS);
+        let x_vel = api_handle.read_memory_f32(XVEL_OFFSETS);
+        let y_vel = api_handle.read_memory_f32(YVEL_OFFSETS);
 
         text.clear();
-        text.push_str(&format!("X: {}\nY: {}\nZ: {}\n{} ({})> {}", x_pos, y_pos, z_pos, position_key, num_mode, self.input_text));
+        text.push_str(&format!(
+            "X: {:.4}\nY: {:.4}\nZ: {:.4}\nXV: {:.4}\nYV: {:.4}\n{} ({}, {})> {}",
+            x_pos, y_pos, z_pos, x_vel, y_vel, position_key, parameter, num_mode, self.input_text
+        ));
 
         let key_pressed = self.get_next_key();
 
-        if key_pressed.is_none() {return;}
+        if key_pressed.is_none() {
+            return;
+        }
 
         match key_pressed.unwrap() {
             Keycode::Key0 | Keycode::Numpad0 => self.input_text.push('0'),
@@ -128,35 +153,66 @@ impl InputHandler {
             Keycode::X => self.position_key = PositionKey::X,
             Keycode::Y => self.position_key = PositionKey::Y,
             Keycode::Z => self.position_key = PositionKey::Z,
-            Keycode::N => self.num_mode = {
-                match self.num_mode {
-                    NumMode::Increase => NumMode::Set,
-                    NumMode::Set => NumMode::Increase,
+            Keycode::N => {
+                self.num_mode = {
+                    match self.num_mode {
+                        NumMode::Increase => NumMode::Set,
+                        NumMode::Set => NumMode::Increase,
+                    }
                 }
-            },
-            Keycode::Backspace => {self.input_text.pop(); ()},
+            }
+            Keycode::P => {
+                self.parameter = {
+                    match self.parameter {
+                        Parameter::Position => Parameter::Velocity,
+                        Parameter::Velocity => Parameter::Position,
+                    }
+                }
+            }
+            Keycode::Backspace => {
+                self.input_text.pop();
+            }
             Keycode::C => self.input_text.clear(),
             Keycode::Enter => {
                 let parsed_str = self.input_text.parse::<f32>();
                 if let Ok(num) = parsed_str {
                     match self.position_key {
-                        PositionKey::X => {
-                            match self.num_mode {
-                                NumMode::Increase => api_handle.write_memory_f32(XPOS_OFFSETS, x_pos + num),
+                        PositionKey::X => match self.parameter {
+                            Parameter::Position => match self.num_mode {
+                                NumMode::Increase => {
+                                    api_handle.write_memory_f32(XPOS_OFFSETS, x_pos + num)
+                                }
                                 NumMode::Set => api_handle.write_memory_f32(XPOS_OFFSETS, num),
-                            }
+                            },
+                            Parameter::Velocity => match self.num_mode {
+                                NumMode::Increase => {
+                                    api_handle.write_memory_f32(XVEL_OFFSETS, x_vel + num)
+                                }
+                                NumMode::Set => api_handle.write_memory_f32(XVEL_OFFSETS, num),
+                            },
                         },
-                        PositionKey::Y => {
-                            match self.num_mode {
-                                NumMode::Increase => api_handle.write_memory_f32(YPOS_OFFSETS, y_pos + num),
+                        PositionKey::Y => match self.parameter {
+                            Parameter::Position => match self.num_mode {
+                                NumMode::Increase => {
+                                    api_handle.write_memory_f32(YPOS_OFFSETS, y_pos + num)
+                                }
                                 NumMode::Set => api_handle.write_memory_f32(YPOS_OFFSETS, num),
-                            }
+                            },
+                            Parameter::Velocity => match self.num_mode {
+                                NumMode::Increase => {
+                                    api_handle.write_memory_f32(YVEL_OFFSETS, y_vel + num)
+                                }
+                                NumMode::Set => api_handle.write_memory_f32(YVEL_OFFSETS, num),
+                            },
                         },
-                        PositionKey::Z => {
-                            match self.num_mode {
-                                NumMode::Increase => api_handle.write_memory_f32(ZPOS_OFFSETS, z_pos + num),
+                        PositionKey::Z => match self.parameter {
+                            Parameter::Position => match self.num_mode {
+                                NumMode::Increase => {
+                                    api_handle.write_memory_f32(ZPOS_OFFSETS, z_pos + num)
+                                }
                                 NumMode::Set => api_handle.write_memory_f32(ZPOS_OFFSETS, num),
-                            }
+                            },
+                            Parameter::Velocity => {}
                         },
                     }
                 }
